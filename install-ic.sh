2138ff2d26005b58388f3b284f68dcf9969e8955
yanghoon <ssang276@hanmail.net>
2019-01-16 11:30:57 +0900

    Create install-ic.sh

---
+++ install-ic.sh
@@ -0,0 +1,5 @@
+# https://console.bluemix.net/docs/cli/reference/ibmcloud/download_cli.html#install_use
+# https://console.bluemix.net/docs/containers/cs_cli_install.html
+
+curl -fsSL https://clis.ng.bluemix.net/install/linux | sh
+ibmcloud plugin install container-service
